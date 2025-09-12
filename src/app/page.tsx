64802ce5530bcec77a7fc1c75cf473af2f6bdf71
Elthecreator7 <enejecaleb@gmail.com>
2025-09-12 13:14:22 +0100

    background jobs set up with inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,23 @@
-import { getQueryClient, trpc } from '@/trpc/server'
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import Client from './Client';
+"use client";
+import { Button } from '@/components/ui/button';
+import { useTRPC } from '@/trpc/client';
+import { useMutation } from '@tanstack/react-query';
 import React, { Suspense } from 'react'
+import { toast } from 'sonner';
 
-const page = async () => {
-  const queryClient = getQueryClient();
+const page =  () => {
+ const trpc = useTRPC();
+ const invoke = useMutation(trpc.invoke.mutationOptions({
+  onSuccess: ()=>{
+    toast.success("background job started b")
+  }
+ }))
 
-  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: "Caleb PREFETCH" }))
 
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback = {<p>Loading...</p>}>
-        <Client></Client>
-      </Suspense>
-    </HydrationBoundary>
+ return (
+    <div className='p-4 max-w-7xl mx-auto'>
+      <Button disabled= {invoke.isPending} onClick={()=> invoke.mutate({text: "Sup bitches"})}>Invoke Background Component</Button>
+    </div>
   )
 }
 
